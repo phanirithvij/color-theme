c7f58a2b44a69460dd76bd6cd00acfcdbc1559c1
Phani Rithvij <phanirithvij2000@gmail.com>
2019-03-29 02:23:21 +0530

    init setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# A simple showcase of image based color theme
+
+### Note
+
+To change `:root` vars in css through js 
+
+```javascript
+document.documentElement.style.setProperty('--your-variable', '#YOURCOLOR');
+```
---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+const gulp = require('gulp');
+const { series, parallel } = gulp;
+const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const typescript = require('gulp-typescript');
+const concat = require('gulp-concat');
+
+var sassDev = ()=>{
+    return gulp.src('src/client/scss/*scss')
+        .pipe(sass())
+        .pipe(gulp.dest('src/server/public/css/'));
+};
+
+var sassProd = ()=>{
+    return gulp.src('src/client/scss/*scss')
+        .pipe(sass({outputStyle:'compressed'}))
+        .pipe(gulp.dest('src/server/public/css/'));
+};
+
+var copyHtml = ()=>{
+    return gulp.src('src/client/html/*.html')
+        .pipe(gulp.dest('src/server/public/'));
+};
+
+var copyJs = ()=>{
+    return gulp.src('src/client/js/*.js')
+        .pipe(gulp.dest('src/server/public/js/'));
+};
+
+var tsloadDev = ()=>{
+    return gulp.src('src/client/**/*.ts')
+        .pipe(typescript())
+        .pipe(concat('prod.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('src/server/public/js/'));
+};
+
+var minifyJs = ()=>{
+	return gulp.src('src/client/js/*.js')
+		.pipe(concat('prod.all.js'))
+        .pipe(gulp.dest('src/server/public/js/'));
+};
+
+var dev = ()=>{
+    gulp.watch(['src/client/scss/*.scss'], sassDev);
+    gulp.watch(['src/client/ts/*.ts', 'src/*.ts'], tsloadDev);
+    gulp.watch(['src/client/js/*.js'], copyJs);
+    gulp.watch(['src/client/html/*.html'], copyHtml);
+};
+
+exports.dev = series(
+    parallel(
+        sassDev,
+        series(tsloadDev, copyJs, minifyJs),
+        copyHtml
+    ),
+    dev
+);
+exports.prod = parallel(
+    sassProd,
+    series(tsloadDev, copyJs, minifyJs),
+    copyHtml
+);
