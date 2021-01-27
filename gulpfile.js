1b0469375dd402683b3184caed34e97d8e896323
phanirithvij <phanirithvij2000@gmail.com>
2021-01-27 22:08:35 +0530

    random fix

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,9 @@ const sass = require('gulp-sass');
 const uglify = require('gulp-uglify');
 const typescript = require('gulp-typescript');
 const concat = require('gulp-concat');
-// const del = require('del');
-// const vpaths = require('vinyl-paths');
+
+
+// TODO add DO NOT EDIT warnings in the generated files
 
 var sassDev = ()=>{
     return gulp.src('src/client/scss/*scss')
